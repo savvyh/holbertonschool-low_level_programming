addcdac09bb91c41eb02e0cad452eb8dd7830f7d
root <7277@holbertonstudents.com>
2023-10-11 13:41:19 -0700

    fix output betty style

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-int size_char = sizeof (char);
-int size_int = sizeof (int);
-int size_longInt = sizeof (long int);
-int size_longLongInt = sizeof (long long int);
-int size_float = sizeof (float);
+int size_char = sizeof(char);
+int size_int = sizeof(int);
+int size_longInt = sizeof(long int);
+int size_longLongInt = sizeof(long long int);
+int size_float = sizeof(float);
 
 printf("Size of a char: %d byte(s)\n", size_char);
 printf("Size of an int: %d byte(s)\n", size_int);
