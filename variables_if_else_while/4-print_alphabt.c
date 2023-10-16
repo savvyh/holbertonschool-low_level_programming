ad8e0aed7b52ab97eff5153d05ff7541122b2216
root <7277@holbertonstudents.com>
2023-10-16 10:41:52 -0700

    alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char character;
+	for (character = 'a'; character <= 'z'; character++)
+
+	if (character != 'q')
+	if (character != 'e')
+
+		putchar(character);
+
+	putchar ('\n');
+
+	return (0);
+}
