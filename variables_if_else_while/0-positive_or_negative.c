59fac4f92d3c9174d8f22f91c391d372d6efbf0e
root <7277@holbertonstudents.com>
2023-10-16 04:38:43 -0700

    variable if, random number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - Number is positive, zero or negative
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	  {
-	    printf("%d is positive\n", n);
-	  }
-	if (n == 0)
-	  {
-	    printf("%d is zero\n", n);
-	  }
-	if (n < 0)
-	  {
-	    printf("%d is negative\n", n);
-	  }
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+Poject C - Variables, if, else, while.
+
+Task 0 : create a program with random number, and display if the number is positive, negative or zero.
+Task 1 : similar to task 0, but print only the last digit of the number stored in the variable.
