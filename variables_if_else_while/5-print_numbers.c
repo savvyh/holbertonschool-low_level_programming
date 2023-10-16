bfb9804ac7cdd20f718cca53438e4e20f8fd0d43
root <7277@holbertonstudents.com>
2023-10-16 12:13:37 -0700

    single digit number of base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - every single digit number
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int number;
+	for (number = '0'; number <= '9'; number++)
+	putchar(number);
+
+  	putchar('\n');
+return (0);
+}
