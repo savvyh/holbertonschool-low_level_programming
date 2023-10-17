8d667650ad32d0aeda8942cef84b62bbcba855bf
root <7277@holbertonstudents.com>
2023-10-16 23:48:03 -0700

    fix betty style base 16 in lowercase

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 int number;
 char alpha;
-for(number = '0'; number <= '9'; number++)
+for (number = '0'; number <= '9'; number++)
 	putchar(number);
-for(alpha ='a'; alpha <='f'; alpha++)
+for (alpha = 'a'; alpha <='f'; alpha++)
 	putchar(alpha);
 putchar('\n');
 return (0);
