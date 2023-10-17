b5275c4da2ab9d2bb0df357404084d2c9a6d2c32
root <7277@holbertonstudents.com>
2023-10-17 00:29:04 -0700

    all combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - all combinaison of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int digit_number;
+for (digit_number = '0'; digit_number <= '9'; digit_number++)
+  {
+	putchar(digit_number);
+
+if (digit_number < '9')
+  {
+	putchar(',');
+	putchar(' ');
+  }
+  }
+return (0);
+}
