58590b414b0f05d7c84858ab07804cde1d7c4429
root <7277@holbertonstudents.com>
2023-10-18 08:08:30 -0700

    Jackk Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,23 @@
 #include "main.h"
 /**
  * jack_bauer - print every minute of day
- * 
  * Return: hour per day
- */
+*/
 void jack_bauer(void)
 {
+	int minute;
+	int hour;
+
+	for (minute = 0 ; minute <= 59 ; minute++)
+	{
+		for (hour = 0 ; hour <= 23 ; hour++)
+		{
+			_putchar(hour / 10);
+			_putchar(hour % 10);
+			_putchar(':');
+			_putchar(minute / 10);
+			_putchar(minute % 10);
+			_putchar('\n');
+		}
+	}
+}
