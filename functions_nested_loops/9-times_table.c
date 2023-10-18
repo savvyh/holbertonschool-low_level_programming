9b0938de03dcfa72113e1e965af6b3803b3c4769
root <7277@holbertonstudents.com>
2023-10-18 09:12:03 -0700

    fix number of character

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,13 +16,21 @@ void times_table(void)
 
 			if (result < 10)
 			{
-				_putchar(' ');
-				_putchar(result + '0');
+				if (figure > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+					_putchar(result + '0');
 			}
 			else
 			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				if (figure > 0)
+				{
+					_putchar(' ');
+				}
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
 			}
 
 			if (figure < 9)
