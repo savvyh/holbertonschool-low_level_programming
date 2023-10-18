ad6ea821b09f74f13f659fb8a4ed7cbcfacb8b8e
root <7277@holbertonstudents.com>
2023-10-18 08:12:21 -0700

    fix ASCII

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 	int minute;
 	int hour;
 
-	for (minute = 0 ; minute <= 59 ; minute++)
+	for (hour = 0 ; hour <= 23 ; hour++)
 	{
-		for (hour = 0 ; hour <= 23 ; hour++)
+		for (minute = 0 ; minute <= 59 ; minute++)
 		{
-			_putchar(hour / 10);
-			_putchar(hour % 10);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar(minute / 10);
-			_putchar(minute % 10);
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
