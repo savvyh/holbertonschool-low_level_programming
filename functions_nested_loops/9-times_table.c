782cb3be1a0858cea93715dd7a3595ad0bef0373
root <7277@holbertonstudents.com>
2023-10-18 09:05:26 -0700

    table 9 times

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ */
+void times_table(void)
+{
+	int multiple;
+	int figure;
+	int result;
+
+	for (multiple = 0; multiple <= 9; multiple++)
+	{
+		for (figure = 0; figure <= 9; figure++)
+		{
+			result = multiple * figure;
+
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+
+			if (figure < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+			_putchar('\n');
+	}
+}
