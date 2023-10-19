e63b807ba17ed7e21bb01a83f264a57fb872aff5
root <7277@holbertonstudents.com>
2023-10-19 05:59:22 -0700

    fix output number > 98 another timer

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -18,10 +18,16 @@ void print_to_98(int n)
 			}
 			n++;
 		}
-		while (n > 98)
-		{
-			printf("%d, ", n);
+			while (n > 98)
+			{
+				if (n != 98)
+				{
+					printf("%d, ", n);
+				}
+				else
+				{
+					printf("%d\n", n);
+				}
 			n--;
-		}
-			printf("%d\n", 98);
+			}
 }
