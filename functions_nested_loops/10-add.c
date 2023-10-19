eb6397658a0ead5ba4d02958849800e5032d9e95
root <7277@holbertonstudents.com>
2023-10-19 04:54:36 -0700

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition of two numbers
+ * @num1: value of number 1
+ * @num2: value of number 2
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
