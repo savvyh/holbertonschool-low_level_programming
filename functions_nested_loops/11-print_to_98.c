f12afb5d9465419fb9387049f74f4478f2f9ee63
root <7277@holbertonstudents.com>
2023-10-19 05:36:29 -0700

    fix output for number > 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - print all natural numbers
+ * print_to_98 - print all natural numbers in n
  * @n: value of number
  */
 void print_to_98(int n)
@@ -17,5 +17,11 @@ void print_to_98(int n)
 				printf("%d\n", n);
 			}
 			n++;
-	}
+		}
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+			printf("%d\n", 98);
 }
