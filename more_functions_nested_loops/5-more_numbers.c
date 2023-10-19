fe7b65fc4172c430338e226d3507d2a25e460c1a
root <7277@holbertonstudents.com>
2023-10-19 15:08:13 -0700

    print numbers from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - 0 to 14 and 10 times
+ */
+void more_numbers(void)
+{
+	int num;
+	int repeat;
+
+	for (repeat = 0; repeat <= 10; repeat++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar(num / 10 + '0');
+			}
+			_putchar(num % 10 + '0');
+
+			if (num == 14)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
