2ef60462d80644100aaf154c7f7751e63b848a13
root <7277@holbertonstudents.com>
2023-10-20 07:00:20 -0700

    print 1 to 100, Fizz and Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print a strange interview
+ * Return: 0 Always
+ */
+int main(void)
+{
+  int number, multiple_3, multiple_5;
+
+	for (number = 1; number <= 100 ; number++)
+	{
+		multiple_3 = number % 3;
+		multiple_5 = number % 5;
+		
+		if (multiple_3 == 0 && multiple_5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (multiple_3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (multiple_5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
+	return (0);
+}
