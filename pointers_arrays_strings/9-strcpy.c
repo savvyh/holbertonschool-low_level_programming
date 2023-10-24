00af771954d7e818881ebc6be2b8e097a4570fe6
root <7277@holbertonstudents.com>
2023-10-24 07:39:19 -0700

    copy the string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - the string
+ * @dest: the pointer
+ * @src: the pointer
+ * Return: to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+	int n = 0;
+
+	for (index = 0; src[index] != '\0'; index++)
+		dest[index] = src[index];
+
+	for ( ; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
