f4da4980c86316622bb2d68db3b95cc44916f9b5
root <7277@holbertonstudents.com>
2023-10-24 01:14:28 -0700

    print in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+
+	_putchar('\n');
+}
