de3979b0cfca78b539338f52820cad3bac2d3543
root <7277@holbertonstudents.com>
2023-10-24 04:44:55 -0700

    print not all the char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print every other character of a string
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+	len++;
+	}
+	_putchar('\n');
+}
