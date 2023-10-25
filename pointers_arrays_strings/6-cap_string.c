ebc25e5ecbe6cb086ebc62bc14da63e81a420b0c
root <7277@holbertonstudents.com>
2023-10-25 13:52:27 -0700

    fix output for an other loop

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,64 +6,21 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
+	char symbol[] = "\t\n ,;.!?'\"'(){}";
+	int i, j;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] == ',')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == ';')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '.')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '!')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '?')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '"')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '(')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == ')')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '{')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-        else if (str[i] == '}')
-        {
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                str[i + 1] = str[i + 1] - 32;
-        }
-
-        i++;
-    }
-
-    return str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			for (j = 0; symbol[j] != '\0'; j++)
+			{
+				if (str[i - 1] == symbol[j])
+				{
+				str[i] = str[i] - 32;
+				}
+			}
+		}
+	}
+	return (str);
 }
-
