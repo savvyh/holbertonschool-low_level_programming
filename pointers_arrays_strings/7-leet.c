9931bbf02502d74a9521ea3c20be9c1d076ca188
root <7277@holbertonstudents.com>
2023-10-26 02:38:25 -0700

    encode a string

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - encode a string
+ * @str: string
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	char encode[] = "43071";
+ 	char letter[] = "aeotl";
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		while (letter[j] != '\0')
+		{
+			if (str[i] == letter[j] || str[i] == letter[j] - 32)
+			{
+				str[i] = encode[j];
+			}
+		}
+		j++;
+
+	i++;
+	}
+
+	return (str);
+}
