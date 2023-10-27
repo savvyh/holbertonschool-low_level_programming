3ba34537ccfe63465dfc1b2f2686a5f6cdaa65c2
root <7277@holbertonstudents.com>
2023-10-27 02:54:50 -0700

    fix other main to find the first occurence of the substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; needle[i] != '\0'; i++)
 	{
 		int j;
 
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; haystack[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i])
+			if (needle[i] == haystack[j])
 			{
 				return (needle);
 			}
