d463a83ebecde7a59c84cd71693b3a03d459b7bf
savvyh <7277@holbertonstudents.com>
2023-11-13 10:56:23 +0000

    fix dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - all about the dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
 #define struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
