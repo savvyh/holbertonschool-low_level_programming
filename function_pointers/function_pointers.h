aa5fa80a109cf65b5afd8d6cf03f93896a00f83a
savvyh <7277@holbertonstudents.com>
2023-11-14 15:53:11 +0000

    fix the .h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -6,5 +7,7 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
