de4055cb3fcd62d7366bd385f85cf187d9ff59a4
savvyh <7277@holbertonstudents.com>
2023-11-14 08:37:05 +0000

    fix output with peers

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
-char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 /**
  * new_dog - create a new dog
@@ -21,17 +21,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(_strlen(name) + 1);
 
 	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
+
+	new_dog->name = _strcpy(new_dog->name, name);
+
+	new_dog->age = age;
 
 	new_dog->owner = malloc(_strlen(owner) + 1);
 
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
+	}
 
-	_strcpy(new_dog->name, name);
-	_strcpy(new_dog->owner, owner);
-
-	new_dog->age = age;
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
