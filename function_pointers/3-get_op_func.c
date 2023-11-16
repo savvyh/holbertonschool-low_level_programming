5b9a90e3bca1724d6c3411d923597ad810b15dcb
savvyh <7277@holbertonstudents.com>
2023-11-16 12:12:57 +0000

    fix this file

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,11 +18,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if ((*ops[i].op) == *s && s[1] == '\0')
+		if (strcmp(s, ops[i].op) == 0)
+		{
 			return (ops[i].f);
-
+		}
 		i++;
 	}
 	return (NULL);
