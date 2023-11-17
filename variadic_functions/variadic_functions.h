e71a8e7bee9168ef306be147b9d31f8b49395e2f
savvyh <7277@holbertonstudents.com>
2023-11-17 17:30:07 +0000

    fix the file.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
