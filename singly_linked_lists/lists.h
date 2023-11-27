bfab583daece91e642a67f874feb5eb8abb5b68d
savvyh <7277@holbertonstudents.com>
2023-11-27 15:29:06 +0000

    fix the order of element I put the structure before prototype

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,6 @@
 #ifndef __LIST__
 #define __LIST__
-
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +16,11 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
