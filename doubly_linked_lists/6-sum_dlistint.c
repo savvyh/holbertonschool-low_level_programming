9de9b8ecf139ccebe1aa31572a862554eca4d92b
savvyh <7277@holbertonstudents.com>
2023-11-29 09:07:23 +0000

    Add a function that summ all data (n) of a list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
