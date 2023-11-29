2e4c920390fd7a8d2bd12d7ad5d7993d2575575e
savvyh <7277@holbertonstudents.com>
2023-11-29 09:10:42 +0000

    fix betty style I miss a decription of the function sum_dlistint

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 /**
+ * sum_dlistint - sum alla data (n) of a list
  *
+ * @head: the head of a list
+ *
+ * Return: the result of this sum
 */
 int sum_dlistint(dlistint_t *head)
 {
