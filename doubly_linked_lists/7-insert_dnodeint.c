7941e814ea5669db2b54244c8e723f2254ba686f
savvyh <7277@holbertonstudents.com>
2023-11-29 14:24:48 +0000

    fix and change my code by using previous function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,42 +11,31 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temporary, *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *temporary = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	temporary = *h;
-
-	if (temporary == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
-		return (new_node);
-	}
 	if (idx == 0)
-	{
-		new_node->prev = NULL;
-		new_node->next = temporary;
-		temporary->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-	while (idx > 1 && temporary->next)
+		return (add_dnodeint(h, n));
+
+	while (idx != 1)
 	{
 		temporary = temporary->next;
+		if (temporary == NULL)
+			return (NULL);
 		idx--;
 	}
-	if (idx > 1 && !temporary->next)
-		return (NULL);
 
+	if (temporary->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node->n = n;
 	new_node->prev = temporary;
 	new_node->next = temporary->next;
-	if (temporary->next)
-		temporary->next->prev = new_node;
+	temporary->next->prev = new_node;
+	temporary->next = new_node;
 
-	temporary->next = NULL;
 	return (new_node);
 }
