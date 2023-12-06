4429bc1a91fbfae7bbd7c2c48d2faac2a2f40a99
savvyh <7277@holbertonstudents.com>
2023-12-06 08:52:13 +0000

    fix and try an other time. I push my personal code but I add a function _strlen inside this file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,33 +1,49 @@
 #include "main.h"
-
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - function that create a file
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 if success or -1 if it failed
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file_descriptor, write_file;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
 		return (-1);
 
-	close(o);
+	if (text_content != NULL)
+	{
+		write_file = write(file_descriptor, text_content, _strlen(text_content));
+		if (write_file == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
 
+	close(file_descriptor);
 	return (1);
+}
+/**
+ * _strlen - the lenght of a string
+ * @s: the string
+ * Return: lenght of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
