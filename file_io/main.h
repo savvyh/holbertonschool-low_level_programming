c930a7c0bcef030c568a3df7962f38f1a98d1ff4
savvyh <7277@holbertonstudents.com>
2023-12-06 16:09:05 +0000

    fix main.h by adding library and erase a prototype

--- file_io/main.h
+++ file_io/main.h
@@ -2,15 +2,15 @@
 #define __MAIN__
 
 #include <sys/types.h>
+#include <sys/uio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-void close_file(int file_descriptor);
 
 #endif
