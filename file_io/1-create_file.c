5145e522e9da0901507b71543bfa85b590d48072
savvyh <7277@holbertonstudents.com>
2023-12-06 08:49:14 +0000

    fix and just try with a new code that's not mine. I try to find the pb with checker

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,33 +1,33 @@
 #include "main.h"
+
 /**
- * create_file - function that create a file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- *
- * Return: 1 if success or -1 if it failed
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_file;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_descriptor == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		write_file = write(file_descriptor, text_content, _strlen(text_content));
-		if (write_file == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(file_descriptor);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
