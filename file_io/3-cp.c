ee7f8822b88c1cf3f0660950d68345c583afc967
savvyh <7277@holbertonstudents.com>
2023-12-06 12:44:52 +0000

    fix by modify dprintf of function create_buffer

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
