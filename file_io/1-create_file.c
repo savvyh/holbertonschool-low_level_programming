7a342695fbc7521a15d0106e316cf4519d538a6d
savvyh <7277@holbertonstudents.com>
2023-12-06 08:47:24 +0000

    fix and modif the order of o-flag CREAT then RDWR

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
