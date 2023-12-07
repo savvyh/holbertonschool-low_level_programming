b5995f6f0a8d09579dff5413892b9ba87be9d867
savvyh <7277@holbertonstudents.com>
2023-12-07 10:28:41 +0000

    fix by came back to my previous code

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -21,7 +21,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
 	for (index = 0; index < size; index++)
 	{
