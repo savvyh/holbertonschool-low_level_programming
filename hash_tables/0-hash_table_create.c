1ac16123f8ded8ece641fb3c060f17ffbea55410
savvyh <7277@holbertonstudents.com>
2023-12-07 07:48:40 +0000

    fix the file 0-hash_table_create by adding the element of the structure and modify the loop

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,15 +10,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(size);
-	unsigned long int index = 0;
+	unsigned int index;
+	hash_table_t *hash_table = malloc(sizeof(hash_node_t));
 
 	if (hash_table == NULL)
 		return (NULL);
 
-	while (index < size)
+	hash_table->size = size;
+	hash_table->array = malloc(size);
+
+	if (hash_table->array == NULL)
 	{
-		index++;
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		hash_table->array = NULL;
 	}
 
 	free(hash_table);
