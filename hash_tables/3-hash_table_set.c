580a233f714040cdb6ec1c091d7f27ab61ba3cec
savvyh <7277@holbertonstudents.com>
2023-12-07 12:31:00 +0000

    add function that adds 1 element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * hash_table_set - function that adds an element to the hash table
+ *
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: the key, which can't be an empty string
+ * @value: the value associated with the key, must be duplicate,
+ *			can be an empty string.
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *new_element = NULL;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_element = malloc(sizeof(hash_node_t));
+	if (new_element == NULL)
+		return (0);
+
+	new_element->key = strdup(key);
+	if (new_element->key == NULL)
+	{
+		free(new_element);
+		return (0);
+	}
+
+	new_element->value = strdup(value);
+	if (new_element->value == NULL)
+	{
+		free(new_element);
+		return (0);
+	}
+
+	new_element->next = ht->array[index];
+	ht->array[index] = new_element;
+
+	return (1);
+}
