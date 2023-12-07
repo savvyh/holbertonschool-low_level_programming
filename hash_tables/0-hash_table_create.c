33720d7a2fa2c2cb20ba0e1b25c8e3506d8fc260
savvyh <7277@holbertonstudents.com>
2023-12-07 07:54:51 +0000

    fix the file 0-hash_table_create by adding a pointeur on the malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_table_t) * size);
+	hash_table->array = malloc(sizeof(hash_table_t *) * size);
 
 	if (hash_table->array == NULL)
 	{
