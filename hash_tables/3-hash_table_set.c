3dcb15ce15b126b83bc51f8426d7c4398a5267ed
savvyh <7277@holbertonstudents.com>
2023-12-08 11:16:29 +0000

    fix and try to came back on my previous code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,34 +15,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new_element = NULL, *current_node = NULL;
+	hash_node_t *new_element = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	current_node = ht->array[index];
-	while (current_node != NULL)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			free(current_node->value);
-			current_node->value = strdup(value);
 
-			if (current_node->value == NULL)
-				return (0);
-			return (1);
-		}
-		current_node = current_node->next;
-	}
 	index = key_index((const unsigned char *)key, ht->size);
+
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 		return (0);
+
 	new_element->key = strdup(key);
 	if (new_element->key == NULL)
 	{
 		free(new_element);
 		return (0);
 	}
+
 	new_element->value = strdup(value);
 	if (new_element->value == NULL)
 	{
@@ -50,7 +40,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_element);
 		return (0);
 	}
+
 	new_element->next = ht->array[index];
 	ht->array[index] = new_element;
+
 	return (1);
 }
