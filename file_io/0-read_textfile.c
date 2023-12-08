54d46bd732d1872c812e98bc229120f68fd538f3
savvyh <7277@holbertonstudents.com>
2023-12-08 15:14:43 +0000

    fix the code

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,16 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
-
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
-		return (0);
-	}
 
 	read_file = read(file_descriptor, buffer, letters);
 	if (read_file == -1)
