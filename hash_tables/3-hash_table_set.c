37b52322cebc62c2af07f6a31b4d15bd0ddfba9d
savvyh <7277@holbertonstudents.com>
2023-12-08 09:05:21 +0000

    fix and now check if current_node->value is egal

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,24 +15,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new_element = NULL;
+	hash_node_t *new_element = NULL, *current_node = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
 
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
 	index = key_index((const unsigned char *)key, ht->size);
-
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 		return (0);
-
 	new_element->key = strdup(key);
 	if (new_element->key == NULL)
 	{
 		free(new_element);
 		return (0);
 	}
-
 	new_element->value = strdup(value);
 	if (new_element->value == NULL)
 	{
@@ -40,9 +50,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_element);
 		return (0);
 	}
-
 	new_element->next = ht->array[index];
 	ht->array[index] = new_element;
-
 	return (1);
 }
